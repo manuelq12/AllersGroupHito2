a19f902a853c9bc2484e5361d177a8b50346d032
david1999erazo <33490042+david1999erazo@users.noreply.github.com>
2018-09-15 22:40:53 -0500

    test error

--- FuerzaBruta/TestAlgoritmos/UnitTestAllers.cs
+++ FuerzaBruta/TestAlgoritmos/UnitTestAllers.cs
@@ -14,7 +14,19 @@ namespace TestAlgoritmos
         [TestMethod]
         public void TestCargarArticulos()
         {
-            String rutaArticulos = "../../Data/Articulos.csv";
+            //String rutaArticulos = "../../Data/Articulos.csv";
+
+
+            try
+            {
+                relacionControlador.cargarArticulosP();
+               Assert.IsTrue(true);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(false);
+            }
+
             
             
 
@@ -24,7 +36,17 @@ namespace TestAlgoritmos
         [TestMethod]
         public void TestCargarClientes()
         {
+           // String rutaCliente = "../../Data/Clientes.csv";
 
+            try
+            {
+                relacionControlador.cargarClientesP();
+                Assert.IsTrue(true);
+            }
+            catch(Exception e)
+            {
+                Assert.IsTrue(false);
+            }
 
 
         }
@@ -32,8 +54,21 @@ namespace TestAlgoritmos
         [TestMethod]
         public void TestCargarVentas()
         {
-    
-                
+
+            //String rutaVentas = "../../Data/Ventas.csv";
+
+            try
+            {
+                relacionControlador.cargarClientesP();
+                Assert.IsTrue(true);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(false);
+            }
+
+
+
 
         }
 
