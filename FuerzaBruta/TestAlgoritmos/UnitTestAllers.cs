6763cc81985d850e98d62260c6b6789a536146e7
david1999erazo <33490042+david1999erazo@users.noreply.github.com>
2018-09-15 19:21:51 -0500

    Creación Clase Test

---
+++ FuerzaBruta/TestAlgoritmos/UnitTestAllers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FuerzaBruta;
+
+namespace TestAlgoritmos
+{
+    [TestClass]
+    public class UnitTestAllers
+    {
+
+        
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            
+
+        }
+    }
+}
