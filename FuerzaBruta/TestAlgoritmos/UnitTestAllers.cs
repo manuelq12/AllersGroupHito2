55f670447f8bf766b37440d7e723e557a9698bce
david1999erazo <33490042+david1999erazo@users.noreply.github.com>
2018-09-28 16:38:13 -0500

    1

--- FuerzaBruta/TestAlgoritmos/UnitTestAllers.cs
+++ FuerzaBruta/TestAlgoritmos/UnitTestAllers.cs
@@ -39,7 +39,7 @@ namespace TestAlgoritmos
             {
                 controlador.CargarClientes();
                 Assert.IsNotNull(controlador.Clientes);
-                
+
             }
             catch
             {
@@ -75,17 +75,17 @@ namespace TestAlgoritmos
             Escenario();
             int[] arreglo = { 1, 2, 3 };
             List<List<int>> listResult = new List<List<int>>();
-            listResult = controlador.CombinacionHasta7(3,arreglo);
+            listResult = controlador.CombinacionHasta7(3, arreglo);
 
             List<int> comExistente = new List<int>() { 1, 3, 2 };
             List<int> combinacion = new List<int>();
 
-            for (int i=0; i<listResult.Count;i++)
+            for (int i = 0; i < listResult.Count; i++)
             {
                 combinacion = listResult[i];
-                for (int j=0; j<combinacion.Count;j++)
+                for (int j = 0; j < combinacion.Count; j++)
                 {
-                    if (combinacion[j]==comExistente[j])
+                    if (combinacion[j] == comExistente[j])
                     {
                         Assert.AreEqual(combinacion[j], comExistente[j]);
                     }
@@ -93,25 +93,24 @@ namespace TestAlgoritmos
                 }
 
             }
-
         }
 
         [TestMethod]
         public void TestRepeticionEnVentas()
         {
-            
-            
+
+
             Escenario();
             controlador.CargarDatos();
-            
+
             List<List<int>> numeros = controlador.CombinacionPrueba();
             List<int> combinacion = new List<int>();
             combinacion = numeros[0];
-            
+
             int cantRepeticiones = 0;
             cantRepeticiones = controlador.RepeticionEnVentasP(combinacion);
             Assert.AreEqual(cantRepeticiones, 33);
-            
+
         }
 
 
@@ -135,29 +134,35 @@ namespace TestAlgoritmos
         {
             Escenario();
 
-            controlador.CargarDatos();
+            //controlador.CargarDatos();
+            controlador.CargarDatosPrueba();
+
             controlador.generarAsociaciones();
             int[] arreglo = { 1, 2, 3 };
             List<List<int>> todo = controlador.CombinacionHasta7(2, arreglo);
             Assert.IsNotNull(todo);
-            List<double> soporte = controlador.soporteAsociaciones(todo,0);
-            Assert.AreEqual(soporte[0],0);
+            List<double> soporte = controlador.soporteAsociaciones(todo, 0);
+            // Assert.AreEqual(soporte[0],0);
+            Assert.AreEqual(soporte[0], 0.8);
 
-           
+
         }
 
         [TestMethod]
         public void TestConfianzaAsocianes()
         {
             Escenario();
-           
-            controlador.CargarDatos();
+
+            controlador.CargarDatosPrueba();
             controlador.generarAsociaciones();
-            
+
+            int[] arreglo = { 1, 2, 3, 4, 5 };
+            List<List<int>> todo = controlador.CombinacionHasta7(4, arreglo);
+            Assert.IsNotNull(todo);
+            List<double> confianza = controlador.confianzaAsociaciones(todo, 1);
+            Assert.AreEqual(confianza[0], 0,75);
+
         }
-
-
-
 
     }
 }
